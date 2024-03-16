0b98d583a2e0b6db6328527dabfab69d18be9417
TaronVardanyan <tarokavardanyan@gmail.com>
2024-03-17 00:07:53 +0400

    add main sidebar

---
+++ app/(dashboard)/_components/Sidebar/components/List/item.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Image from "next/image";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+import { cn } from "@/lib/utils";
+import Hint from "@/components/hint";
+
+interface ItemProps {
+    id: string;
+    name: string;
+    imageUrl: string;
+}
+
+const Item = ({ id, name, imageUrl }:ItemProps) => {
+    const { organization } = useOrganization();
+    const { setActive } = useOrganizationList();
+    const isActive = organization?.id === id;
+    
+    const onClick = () => {
+        if(!setActive) return;
+        setActive({ organization: id });
+    }
+    
+    return (<div className="aspect-square relative">
+        <Hint
+            label={name}
+            side="right"
+            align="start"
+            sideOffset={18}
+        >
+            <Image
+                fill
+                src={imageUrl}
+                onClick={onClick}
+                alt={name}
+                className={cn(
+                    "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
+                    isActive && "opacity-100"
+                    )}
+            />
+        </Hint>
+    </div>);
+}
+
+export default Item;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [{
+            protocol: "https",
+            hostname: "img.clerk.com",
+        }]
+    }
+};
 
 export default nextConfig;
