dbcf79d627a2da37d0aac85412148526e45ad898
TaronVardanyan <tarokavardanyan@gmail.com>
2024-04-01 14:09:10 +0400

    add card actions

---
+++ components/actions.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import { ReactNode } from "react";
+import ConfirmModal from "@/components/confirm-modal";
+import { Link2, Trash2 } from "lucide-react";
+import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import {DropdownMenuContentProps} from "@radix-ui/react-dropdown-menu";
+import {toast} from "sonner";
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+
+interface ActionsProps {
+    id: string,
+    title: string,
+    children: ReactNode,
+    side?: DropdownMenuContentProps["side"],
+    sideOffset?: DropdownMenuContentProps["sideOffset"],
+}
+
+const Actions = ({
+    id,
+    title,
+    side,
+    children,
+    sideOffset,
+}:ActionsProps) => {
+    const { mutate, pending } = useApiMutation(api.board.remove);
+    const onCopyLink = () => {
+        navigator.clipboard.writeText(`${window.location.origin}/board/${id}`)
+        .then(() => toast.success('Link copied'))
+        .catch(() => toast.error("Failed to copy link"));
+    };
+    
+    const onDelete = async () => {
+        await mutate({ id })
+        .then(() => toast.success('Board deleted'))
+        .catch(() => toast.error("Failed to delete board"));
+    }
+    
+    return (<DropdownMenu>
+        <DropdownMenuTrigger>
+            {children}
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+            onClick={e => e.stopPropagation()}
+            side={side}
+            sideOffset={sideOffset}
+            className="w-60"
+        >
+            <DropdownMenuItem
+                className="p-3 cursor-pointer"
+                onClick={onCopyLink}
+            >
+                <Link2 className="h-4 w-4 mr-2"/>
+                Copy board link
+            </DropdownMenuItem>
+            <DropdownMenuSeparator/>
+            <ConfirmModal
+                header="Delete board?"
+                description="This will delete the board and all of its contents."
+                disabled={pending}
+                onConfirm={onDelete}
+            >
+                <Button
+                    variant="ghost"
+                    className="p-3 cursor-pointer text-sm w-full justify-start font-normal"
+                >
+                    <Trash2 className="h-4 w-4 mr-2"/>
+                    delete board
+                </Button>
+            </ConfirmModal>
+        </DropdownMenuContent>
+    </DropdownMenu>)
+}
+
+export default Actions;
---
+++ components/confirm-modal.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogContent,
+    AlertDialogCancel,
+    AlertDialogTrigger,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+} from "@/components/ui/alert-dialog"
+
+interface ConfirmModalProps {
+    children: ReactNode,
+    onConfirm: () => void;
+    disabled?: boolean;
+    header: string;
+    description?: string;
+}
+
+const ConfirmModal = ({ children, onConfirm, disabled, header, description }:ConfirmModalProps) => {
+    return (<AlertDialog>
+        <AlertDialogTrigger asChild>
+            {children}
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+            <AlertDialogHeader>
+                <AlertDialogTitle>
+                    {header}
+                </AlertDialogTitle>
+                <AlertDialogDescription>
+                    {description}
+                </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction
+                    disabled={disabled}
+                    onClick={onConfirm}
+                >
+                    Confirm
+                </AlertDialogAction>
+            </AlertDialogFooter>
+        </AlertDialogContent>
+    </AlertDialog>);
+}
+
+export default ConfirmModal;
