d8f164ebb837155c2cbea5a6dff1e92dd35ce931
TaronVardanyan <tarokavardanyan@gmail.com>
2024-04-03 12:14:18 +0400

    add favoriting feature

--- app/(dashboard)/_components/BoardCard/footer.tsx
+++ app/(dashboard)/_components/BoardCard/footer.tsx
@@ -1,3 +1,4 @@
+import { MouseEvent, MouseEventHandler } from "react";
 import { Star } from "lucide-react";
 import { cn } from "@/lib/utils";
 
@@ -18,28 +19,34 @@ const Footer = ({
     disabled,
     onClick,
 }:FooterProps) => {
-    return <div className="relative bg-white p-3">
+    const handleClick: MouseEventHandler<HTMLButtonElement> = (event: MouseEvent<HTMLButtonElement>) => {
+        event.stopPropagation();
+        event.preventDefault();
+        onClick();
+    };
+
+    return (<div className="relative bg-white p-3">
         <p className="text-[13px] truncate max-w-[calc(100%-20px)]">{title}</p>
         <p className="opacity-0 group-hover:opacity-100 transition-opacity text-[11px] text-muted-foreground truncate">
             {authorLabel}, {createdAtLabel}
         </p>
         <button
             disabled={disabled}
-            onClick={onClick}
+            onClick={handleClick}
             className={
             cn(
                 "opacity-0 group-hover:opacity-100 transition absolute top-3 right-3 text-muted-foreground hover:text-blue-600",
                 disabled && "cursor-not-allowed opacity-75"
-            )}
-        >
+                )}
+            >
             <Star
                 className={cn(
                     "h-4 w-4",
                     isFavorite && "fill-blue-600 text-blue-600"
-                )}
+                    )}
             />
         </button>
-    </div>
+    </div>)
 };
 
 export default Footer;
--- app/(dashboard)/_components/BoardCard/index.tsx
+++ app/(dashboard)/_components/BoardCard/index.tsx
@@ -9,6 +9,9 @@ import { useAuth } from "@clerk/nextjs";
 import { Skeleton } from "@/components/ui/skeleton";
 import Actions from "@/components/actions";
 import { MoreHorizontal } from "lucide-react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 
 interface BoardCardProps {
     id: string;
@@ -21,13 +24,37 @@ interface BoardCardProps {
     createdAt: number;
 }
 
-const BoardCard = ({ id, orgId, authorId, title, authorName, imageUrl, createdAt, isFavorite }:BoardCardProps) => {
+const BoardCard = ({
+    id,
+    orgId,
+    authorId,
+    title,
+    authorName,
+    imageUrl,
+    createdAt,
+    isFavorite
+}:BoardCardProps) => {
+    const { mutate: onFavorite, pending: pendingFavorite } = useApiMutation(api.board.favorite);
+    const { mutate: onUnfavorite, pending: pendingUnfavorite } = useApiMutation(api.board.unfavorite);
     const { userId } = useAuth();
     const authorLabel = userId === authorId ? "You" : authorName;
     const createdAtLabel = formatDistanceToNow(createdAt, {
         addSuffix: true,
     });
-        
+
+    const toggleFavorite = async () => {
+        if(isFavorite) {
+            await onUnfavorite({ id })
+            .catch(() => toast.error("Failed to unfavorite"));
+        } else {
+            await onFavorite({
+                id,
+                orgId,
+            })
+            .catch(() => toast.error("Failed to favorite"));
+        }
+    }
+
     return <Link href={`/board/${id}`}>
         <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between align-center overflow-hidden">
             <div className="relative flex-1 bg-amber-50">
@@ -57,8 +84,8 @@ const BoardCard = ({ id, orgId, authorId, title, authorName, imageUrl, createdAt
                 title={title}
                 authorLabel={authorLabel}
                 createdAtLabel={createdAtLabel}
-                onClick={() => {}}
-                disabled={false}
+                onClick={toggleFavorite}
+                disabled={pendingFavorite || pendingUnfavorite}
             />
         </div>
     </Link>
