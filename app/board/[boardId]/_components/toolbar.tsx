8b6114c6991c19b34be50528406ed859aa43fc2b
TaronVardanyan <tarokavardanyan@gmail.com>
2024-04-03 14:10:49 +0400

    add canvas layout

---
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -0,0 +1,15 @@
+const Toolbar = () => {
+    return <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
+        <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
+            <div>pencil</div>
+            <div>square</div>
+            <div>circle</div>
+        </div>
+        <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
+            <div>Undo</div>
+            <div>Redo</div>
+        </div>
+    </div>
+}
+
+export default Toolbar;
